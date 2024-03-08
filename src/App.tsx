2682a24bd9e2921af7a44c75362fe1f49d4250bd
iman fauzi <imanfauzi29@gmail.com>
2024-03-08 11:43:57 +0700

    Initial config

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+    <div className="h-screen w-full">
+      <div className="flex flex-col justify-between items-center">
+        <img src="/logo.webp" className="w-20" />
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
